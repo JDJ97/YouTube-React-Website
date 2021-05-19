1d61d58e4918415eef2b8a9441c5ebf2ff93f3d2
poonam <poonam398@gmail.com>
2021-05-18 22:10:33 -0400

    API

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -7,7 +7,10 @@ const Home = () => {
 
     const FetchVideos = async () => {
         try {
-           const res = await axios.get("") 
+        const res = await axios.get(`https://www.googleapis.com/youtube/v3/videos?part=player&id=aqz-KE-bpKQ&key=AIzaSyAQv4WQq76MzwsBnlmtlhujCeP4Ph2qlC8`)
+        debugger
+           console.log(res.data.player)
+           setVideos(res.data.items[0].player.embedHtml)
         } catch (error) {
             setVideos([])
         }
@@ -17,12 +20,19 @@ const Home = () => {
         FetchVideos()
     }, [])
 
+    const handleSubmit =(e)=>{
+        e.preventDefault()
+    }
+
     return (
+        
         <section className="homeContainer">
-            <form>
+            <form onSubmit={handleSubmit}>
                 <input/>
                 <button>Submit</button>
             </form>
+          {/* {videos} */}
+          <iframe width="480" height="270" src="//www.youtube.com/embed/aqz-KE-bpKQ" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" title="Bunny" allowfullscreen></iframe>
 
         </section>
     )
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -4,9 +4,9 @@ import "./NavBar.css"
 const NavBar = () => {
     return (
         <nav className="NavBarContainer">
-            <img />
-            <NavLink to="/">Home</NavLink>
             <NavLink to="/about">About</NavLink>
+            <NavLink to="/">Home</NavLink>
+            <img src="https://variety.com/wp-content/uploads/2020/06/youtube-logo.png" alt="Youtube Logo"/>
         </nav>
     )
 }
