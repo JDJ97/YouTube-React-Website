3f20365982c79c6158ab26ba8931b25516d6df94
JDJ97 <jdj97@techie.com>
2021-05-20 19:10:49 -0400

    Call api for video

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import NavBar from "./Components/NavBar"
 import {Switch, Route} from "react-router-dom"
 import Home from "./Components/Home"
 import About from "./Components/About"
-
+import Videos from "./Components/Videos"
 const App = () => {
   return (
     <div className="App">
@@ -11,6 +11,7 @@ const App = () => {
       <Switch>
       <Route exact path="/" component={Home} />
       <Route path="/about" component={About} />
+      <Route path="/video/id" component={Videos} />
       </Switch>
     </div>
   );
--- src/Components/Videos.js
+++ src/Components/Videos.js
@@ -1,4 +1,28 @@
-import React from "react"
-
+import React, { useState, useEffect } from "react"
+import axios from "axios"
+const Video = (props) => {
+    const [video, setVideo] = useState([])
+    const fetchVideo = async () =>{
+        try {
+            const { id } = props.match.params
+            const res = await axios.get(`https://youtube.googleapis.com/youtube/v3/videos?id=${id}&key=${process.env.REACT_APP_API_KEY}`)
+            console.log(res)
+        } catch (error) {
+            setVideo([])
+        }
+    }
+   useEffect(() => {
+        fetchVideo()
+    }, [])
+    const goBack = () => {
+        props.history.goBack();
+    }
+    return (
+        <section>
+            <button onClick={goBack}>Return</button>
+        </section>
+    )
+}
+export default Video;
 //props that contains videoID
 //use id to make video api call
