3fd80e86872ee2e6977235db72201be1c085aa8b
Yashiry Azize <azizeyashiry@gmail.com>
2021-05-20 19:01:50 -0400

    api call for video

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import NavBar from "./Components/NavBar"
 import {Switch, Route} from "react-router-dom"
 import Home from "./Components/Home"
 import About from "./Components/About"
+import Video from "./Components/Video"
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Switch>
       <Route exact path="/" component={Home} />
       <Route path="/about" component={About} />
+      <Route path="/video/:id" component={Video} />
       </Switch>
     </div>
   );
--- src/Components/Videos.js
+++
@@ -1,4 +0,0 @@
-import React from "react"
-
-//props that contains videoID
-//use id to make video api call
